3f9ddbf81f5b45d5b62b8299ad9bb0e9186b4120
Tabitha Lyn <tabithalyn6@gmail.com>
2023-08-30 14:37:37 -0400

    Explore sparkles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="bg-darkeggplant h-[90vh] flex justify-center items-center text-latte">
-      <h1 className="text-5xl font-lora italic">explore</h1>
+      <h1 className="text-5xl font-lora italic hover:bg-[url('src/assets/5Xyg.gif')] bg-cover py-8 transition-all hover:cursor-default">explore</h1>
       <h3 className="ml-4 font-karla text-xl font-light">the Wizarding World</h3>
     </div>
   );
